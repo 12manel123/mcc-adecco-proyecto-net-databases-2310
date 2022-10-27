c205237e48e7adf4d0ee7bceb6a8c439566e790a
12manel123 <manelcastellvicerezuela@gmail.com>
2022-10-27 10:22:25 +0200

    Guardar Libros a Db MySQL

--- EFC1/Models/Book.cs
+++ EFC1/Models/Book.cs
@@ -1,8 +1,13 @@
 ﻿namespace EFC1.Models;
-public class Book{//exactamenten el mismo name
+public class Book {//exactamenten el mismo name
     [Key]
     public int Id { get; set; }
     public string Title { get; set; }
     public string Isbn { get; set; }
+
+    public override string ToString() {
+        return $"Book: {Id}, Title:{Title}, Isbn:{Isbn}";
+    }
+
 
 }
--- EFC1/Program.cs
+++ EFC1/Program.cs
@@ -1,1 +1,29 @@
 ﻿Console.WriteLine("Hello, World!");
+
+/*var factory = new AppDbContextFactory();
+var context = factory.CreateDbContext(null);*/
+var context = new AppDbContextFactory().CreateDbContext(null);
+
+Console.WriteLine("================================= GUARDAR UNO ====================");
+Book book1 = new Book { Isbn = "123456ER", Title = "DOnKijte de la Almeria" };
+Book book2 = new Book { Isbn = "9876544ER", Title = "Santmaria de la coloma" };
+
+context.Books.Add(book1);
+context.Books.Add(book2);
+
+context.SaveChanges();
+Console.WriteLine(book1);
+Console.WriteLine(book2);
+
+
+Console.WriteLine("================================= GUARDAR VARIOS ====================");
+Book book3 = new Book { Isbn = "2353242R", Title = "book3" };
+Book book4 = new Book { Isbn = "357534V", Title = "book4" };
+var books = new List<Book> { book3,book4};
+
+context.Books.AddRange(books);
+
+context.SaveChanges();
+
+Console.WriteLine(book3);
+Console.WriteLine(book4);
